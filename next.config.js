211d82756fca83f9aa25394411b142ae6e422f9e
Titilayo <titilayo@tegbrum03672.home>
2024-08-19 20:12:26 +0100

    disable image optimization

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
   basePath: "/URL-shortening-API-landing-page",
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
